d26d06db2d31262cd4136742cd9645de419de159
shaliniramesh1504 <34571423+shaliniramesh1504@users.noreply.github.com>
2018-09-07 14:25:30 +0530

    Create multiples.c

---
+++ multiples.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        int sum=0,i,n;
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("multiple of %d is %d.",i,5*i);
+        }
+}
+
+
